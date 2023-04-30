c4a2e9669c1585188136a44eca0e75be7f66131a
DokermanD <Dmitry.Hatkevich@gmail.com>
2023-04-30 03:11:48 +0300

    Добавьте файлы проекта.

---
+++ Homework-13/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Homework_13
+{
+    public class Person
+    {
+        public static List<Person> listPerson = new List<Person>();
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name, int age) 
+        {
+            Age = age;
+            Name = name;
+            listPerson.Add(this);
+        }
+
+        public static void SetNewPerson()
+        {
+            int nom = 0;
+            for (int i = 18; i < 26; i++)
+            {
+                nom++;
+                var name = "person" + nom;
+                var age = i;
+                new Person(name, age);
+
+            }
+        }
+    }
+}
---
+++ Homework-13/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Homework_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var iii = list.Top(30);
+
+            Person.SetNewPerson();
+            
+            Person.listPerson.Top(30, person => person.Age);
+
+        
+        }
+    }
+
+    public static class MyExtentions
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> colection, int value)
+        {
+            List<T> rezult = new List<T>();
+            if (value > 1 && value < 101)
+            {
+                int collElements = (int)Math.Ceiling((double)colection.Count() / 100 * value);
+                rezult = colection.OrderByDescending(x => x).Take(..collElements).ToList();
+
+                Console.WriteLine(string.Join(", ", rezult));
+                Console.WriteLine("-----------------");
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            return rezult;
+        }
+
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> colection, int value, Func<T,int> func)
+        {
+            List<T> rezult = new List<T>();
+            if (value > 1 && value < 101)
+            {
+                int collElements = (int)Math.Ceiling((double)colection.Count() / 100 * value);
+                rezult = colection.OrderByDescending(func).Take(..collElements).ToList();
+
+                for (int i = 0; i < rezult.Count; i++)
+                {
+                    var nameProp = typeof(Person).GetProperty("Name");
+                    var ageProp = typeof(Person).GetProperty("Age");
+                    var name = nameProp?.GetValue(rezult[i]);
+                    var age = ageProp?.GetValue(rezult[i]);
+                    Console.WriteLine($"{name} - {age}");
+                } 
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            return rezult;
+        }
+    }
+}
